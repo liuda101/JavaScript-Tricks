27be049af63ecc0671a6a2c38c5e0ea5a43125d3
刘奇峰 <qfliu@bainainfo.com>
2013-03-27 11:45:15 +0800

    Add function-override-with-wrap

---
+++ function-override-with-wrap/src.js
@@ -0,0 +1,14 @@
+function wrap(object,method,wrapper){
+	var fn = object[method];
+	return object[method] = function(){
+		return wrapper.apply(this,[fn.bind(this)].concat(Array.prototype.slice.call(arguments)));
+	};
+}
+
+
+// example
+if(Prototype.Browser.Opera){
+	wrap(Element.Methods,'readAttribute',function(original,elem,attr){
+		return attr == 'title' ? elem.title : original(elem,attr);
+	});
+}
